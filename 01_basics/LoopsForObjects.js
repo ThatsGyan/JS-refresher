d8267f6b2e3eafd049d6c169d75447583e98bf41
ThatsGyan <gyansat1399@gmail.com>
2024-07-22 18:57:06 +0530

    DOM

--- 01_basics/LoopsForObjects.js
+++ 01_basics/LoopsForObjects.js
@@ -1,10 +1,25 @@
 const myObject = {
-    js : 'javaScript',
-    cpp: 'C++',
-    rb : 'ruby',
-    swift : 'swift by apple'
+  js: "javaScript",
+  cpp: "C++",
+  rb: "ruby",
+  swift: "swift by apple",
+};
+
+for (const key in myObject) {
+  console.log(myObject[key]); //Result :
 }
 
-for (const key in object) {
+const programming = ["js", "rb", "c++", "java"];
+for (const key in programming) {
+    console.log(programming[key]); //Result : 
+}
+
+const map = new Map()
+
+map.set('IN', 'India')
+map.set('USA', 'United state of America')
+map.set('Fr','France')
+
+for (const key in map) {
     console.log(key); //Result : 
 }
